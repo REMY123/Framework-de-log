a863edb12b31c37e8de45528f996bbc0901f5fee
bayakissa <lanouvell@hotmail.fr>
2015-02-25 05:21:49 +0100

    Message + Formater (some changes)

---
+++ src/fr/logger/com/Message.java
@@ -0,0 +1,22 @@
+package fr.logger.com;
+
+public class Message extends Logger{
+	
+	private String message;
+
+	public String getMessage(){
+		return message;
+	}
+	
+	public void setMessage(String newmessage) {
+		message = newmessage;
+	}
+	
+	public Message()
+	{
+		//je ne sais pas encore bien ce que je dois mettre la dedans...
+	}
+	
+	
+}
+
